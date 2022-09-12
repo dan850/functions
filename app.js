25f70c6598ea4c6c305f4ef58a5e9807e089bef1
dan850 <dan06982@gmail.com>
2022-09-12 06:12:16 -0500

    changes

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-//const d = new Date();
-document.getElementById("demo").innerHTML = monday(4,3);
+const redSoccerPlayer = {
+    firstName: "Daniel",
+    lastName: "Acosta",
+    playerNumber: 30,
+    goals: 5,
+    fullName: function() {
+      return this.firstName + " " + this.lastName + "<br> " 
+      + "PLAYER NUMBER: " + " " + this.playerNumber;
+
+    }
+
+  };
+
+  const blueSoccerPlayer = {
+    firstName: "Valerie",
+    lastName: "Smith",
+    playerNumber: 21,
+    goals: 4,
+    fullName: function() {
+      return this.firstName + " " + this.lastName + "<br> " 
+      + "PLAYER NUMBER: " + " " + this.playerNumber;
+
+    }
+
+  };
+
+  let x = blueSoccerPlayer.goals;
+
+  document.getElementById("demo4").innerHTML = x;
+  
+  document.getElementById("demo").innerHTML = monday(4,x);
+document.getElementById("demo2").innerHTML = redSoccerPlayer.fullName();
+document.getElementById("demo3").innerHTML = blueSoccerPlayer.fullName();
+
 
 
 
@@ -17,7 +49,6 @@ return ActiveHours;
 
 
 function tuesday() {
-
 
 
 }
