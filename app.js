107d480f927ed3134770c6a7709b25a06200bdff
dan850 <dan06982@gmail.com>
2022-09-12 01:46:56 -0500

    code upload

---
+++ app.js
@@ -0,0 +1,50 @@
+//const d = new Date();
+document.getElementById("demo").innerHTML = monday(4,3);
+
+
+
+
+
+function monday(work, play) {
+
+
+let ActiveHours = work * play;
+//console.log(ActiveHours)
+
+return ActiveHours;
+
+}
+
+
+function tuesday() {
+
+
+
+}
+
+
+function wedensday() {
+
+
+
+}
+
+function thursday() {
+
+
+
+}
+
+
+function friday() {
+
+
+
+}
+
+
+function saturday() {
+
+
+
+}
